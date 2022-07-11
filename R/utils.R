193b5c780b0206428d162e55bfdc3bfb7ab7a8ce
Maxwell Palmer <maxwellpalmer@gmail.com>
2022-07-10 21:07:03 -0400

    Revert "drop_na_cols"
    This reverts commit 782b213213215b78b9df637aeb6b74c3846cddf1.

--- R/utils.R
+++ R/utils.R
@@ -83,9 +83,8 @@ pr <- function(x) {
 #' x <- data.frame(a=1:5, b=1:5, c=NA, d=NA, e=letters[1:5])
 #' drop_na_cols(x, verbose=TRUE)
 drop_na_cols <- function(x, verbose=FALSE) {
-  to_drop <- colSums(is.na(x))==nrow(x)
   if(verbose==TRUE) {
-    message(paste("Dropping columns:", paste(names(x)[to_drop], collapse=", ")))
+    message(paste("Dropping columns:", paste(names(x)[colSums(is.na(x))==nrow(x)], collapse=", ")))
   }
-  x[ , !to_drop]
+  x[ , colSums(is.na(x)) < nrow(x)]
 }
