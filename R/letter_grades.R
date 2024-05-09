a47a552697612075b6efd7fff9b7d123e6b2a93b
Maxwell Palmer <maxwellpalmer@gmail.com>
2024-05-09 09:27:06 -0400

    letter_grades_to_pct

--- R/letter_grades.R
+++ R/letter_grades.R
@@ -19,3 +19,19 @@ letter_grades <- function(scores, vals=c(0.93, 0.90, 0.87, 0.83, 0.80, 0.77, 0.7
 
   factor(unlist(lapply(scores, function(i) labs[which.max(i>=vals)])), levels=labs)
 }
+
+
+#' Calculate percentages from letter grades
+#'
+#' @param grades A vector of letter grades
+#' @param vals Optional. A vector of the percentage value for each letter grade.
+#' @param labs Optional. A vector of the names of each grade, sorted from best to worse. Must be the same length as `vals`.
+#'
+#' @return A vector of percentages.
+#' @export
+#'
+letter_grades_to_pct <- function(grades, vals=c(1, .92, .89, 0.86,  0.82, 0.79,  0.76,  0.72,  0.69,  0.59),
+                          labs=c("A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "F")) {
+  if(length(vals)!=length(labs)) stop("vals and labs must have the same length.")
+  vals[unlist(lapply(grades, function(i) which.max(i==labs)))]
+}
