4cf3824ae5c081b2ff660902ebb5ca2eeb341cf0
maxwellpalmer <maxwellpalmer@gmail.com>
2025-09-26 13:37:52 -0400

    Fix mp_theme due to changes in ggplot2.

--- R/mp_theme.R
+++ R/mp_theme.R
@@ -26,7 +26,7 @@ theme_mp <- function(base_family="Arial Narrow", base_size=12, grid="minor") {
   g <- g + ggplot2::theme(panel.spacing.x=unit(.5, "lines"),
                           panel.spacing.y=unit(.5, "lines"),
                           strip.text=element_text(size=10),
-                          panel.border = element_rect(color="gray40", fill=NA, size=.25))
+                          panel.border = element_rect(color="gray40", fill=NA, linewidth=.25))
   if(grid =="none") {
     g <- g + ggplot2::theme(panel.grid.major.x = element_blank(),
                    panel.grid.major.y = element_blank(),
@@ -82,4 +82,49 @@ theme_pres <- function(base_size=32, base_family="Montserrat") {
   g
 }
 
+#' Reasonable Colors palette generator
+#'
+#' @param color Starting color for palette. One of "rose", "raspberry", "red", "orange", "cinnamon", "amber", "yellow", "lime", "chartreuse", "green", "emerald", "aquamarine", "teal", "cyan", "powder", "sky", "cerulean", "azure", "blue", "indigo", "violet", "purple", "magenta", "pink".
+#' @param shade An integer from 1 (lightest) to 6 (darkest). Default is 3.
+#' @param pairing One of "complementary", "split_complementary", "triadic", "tetradic", "analogous", or "set6".
+#'
+#' @return A named vector of html color codes.
+#' @export
+#'
+#' @examples
+#' rcols.pallette("green", shade=2, pairing="triadic")
+#'
+rcols.pallette <- function(color, shade=3, pairing="complementary") {
+  if(is.null(color)) stop("`color' must be specified.")
+  if(! color %in% rcols.pairings$lab) stop("Invalid `color`.")
+  if(!all(is.numeric(shade)) | ! all(shade %in% seq(1:6)) | length(shade)>1) stop("`shade` must be an integer from 1 to 6.")
+  if(! pairing %in% names(rcols.pairings)[2:7]) stop(paste0("`pairing` must be one of: ", glue::glue_collapse(names(rcols.pairings)[2:7], sep=", ", last=", or ")))
+
+  palmer::rcols[paste0(rcols.pairings[rcols.pairings$lab==color, pairing][[1]] |> unlist(), shade)]
+}
+
+rcols.pallette.print <- function(color=NULL, shade=NULL, pairing="complementary") {
+  if(is.null(shade)) shade <- 1:6
+  if(!all(is.numeric(shade))) stop("`shade` must be one or more integers from 1 to 6.")
+  shade <- sort(unique(shade))
+  if(is.null(color)) color <- rcols.pairings$lab
+  if(!all(color %in% rcols.pairings$lab)) stop("One or more invalid values in `color`.")
+  if(! pairing %in% names(rcols.pairings)[2:7]) stop(paste0("`pairing` must be one of: ", glue::glue_collapse(names(rcols.pairings)[2:7], sep=", ", last=", or ")))
+
+  x <- tidyr::expand_grid(shade=shade, color=color) |>
+    mutate(pal=lapply(1:n(), function(i) rcols.pallette(color[i], shade[i], pairing))) |>
+    tidyr::unnest(cols=pal) |>
+    dplyr::mutate(x=dplyr::row_number(), .by=c(shade, color))
+
+  p <- ggplot2::ggplot(x, ggplot2::aes(x=x, y=color)) +
+        ggplot2::geom_tile(fill=x$pal, color="black") +
+        ggplot2::facet_wrap(~shade, nrow = 1) +
+        ggplot2::coord_fixed(ratio=.5) +
+        ggplot2::labs(x=NULL, y=NULL) +
+        ggplot2::theme_minimal() +
+        ggplot2::theme(axis.text.x = ggplot2::element_blank())
+
+  if(!interactive()) return(p)
+  plot(p)
+}
 
